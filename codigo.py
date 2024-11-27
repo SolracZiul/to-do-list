82bab7dd2d7006995da951e739113cff176c13a8
unknown <SolracZiul>
2024-11-27 19:29:37 -0300

    Adicionar a função exibir-menu

---
+++ codigo.py
@@ -0,0 +1,7 @@
+def exibir_menu():
+    print("\n--- To-Do List ---")
+    print("1. Adicionar Tarefa")
+    print("2. Listar Tarefas")
+    print("3. Marcar Tarefa como Concluída")
+    print("4. Remover Tarefa")
+    print("5. Sair")
