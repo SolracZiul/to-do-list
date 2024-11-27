1c194c6c0989e4b4fbd1574fbc91a344cbcdfe7f
unknown <SolracZiul>
2024-11-27 19:39:18 -0300

    Adiciona a função adicionar-tarefa

--- codigo.py
+++ codigo.py
@@ -5,3 +5,8 @@ def exibir_menu():
     print("3. Marcar Tarefa como Concluída")
     print("4. Remover Tarefa")
     print("5. Sair")
+
+def adicionar_tarefa(lista):
+    tarefa = input("Digite a nova tarefa: ")
+    lista.append({"tarefa": tarefa, "concluida": False})
+    print(f"Tarefa '{tarefa}' adicionada com sucesso!")
